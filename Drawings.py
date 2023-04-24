56e274eaff53d22bf93d593b1da9bd4939df5913
Mishos-untied <mvalexan@andrew.cmu.edu>
2023-04-23 22:35:29 -0400

    fun

--- Drawings.py
+++ Drawings.py
@@ -52,3 +52,35 @@ def drawLander(app,height, dx, dy):
     drawLine(cX-dx,cY+width/3-dy,cX-dx,cY+height/3-dy, lineWidth=3)
 
     drawLine(cX-width/8-dx,cY+height/3-dy,cX+width/8-dx,cY+height/3-dy, lineWidth=6)
+
+def drawLaunchRocket(app,height, dx, dy, cX=None, cY=None, engineOn=False):
+    width = height/18
+    if cX==None:
+        cX = app.rocket.position.x
+    if cY ==None:
+        cY = app.rocket.position.y
+    #mainFuselage
+    drawRect(cX-width/2-dx, cY-height/3-dy, width, height/1.5, fill='snow', border=None)
+    CSMWidth = width
+    CSMHeight = CSMWidth *4
+    drawCSM(app, height=CSMHeight, dx=dx, dy=dy, cX=cX, cY=cY-height/2.5, engineOn=False)
+    drawRect(cX-width/2-dx, cY-(height/2.75)-dy, CSMWidth, CSMHeight/4, fill='dimGray')
+    drawPolygon(cX-width/2-dx,cY+(height/3)-(width/10)-dy,cX-width/2-dx,cY+(height/3)-height/15-dy,cX-width/2-width/1.6-dx,cY+(height/3)-(width/2)-dy,cX-width/2-width/1.6-dx,cY+(height/3)-(width/10)-dy,fill='darkGray')
+    drawPolygon(cX+width/2-dx,cY+(height/3)-(width/10)-dy,cX+width/2-dx,cY+(height/3)-height/15-dy,cX+width/2+width/1.6-dx,cY+(height/3)-(width/2)-dy,cX+width/2+width/1.6-dx,cY+(height/3)-(width/10)-dy,fill='darkGray')
+    drawLine(cX-dx,cY+(height/3)-(width/10)-dy,cX-dx,cY+(height/3)-height/15-dy, lineWidth=width/9, fill='darkGray')
+    drawLine(cX-width/2.5-dx,cY+(height/2.976)-dy,cX+width/2.5-dx,cY+(height/2.976)-dy,lineWidth=width/25,fill='silver')
+    drawArc(cX-width/5-dx, cY+(height/2.81)-dy, width/3.5, width/1.6,270,90, fill = 'dimGray')
+    drawArc(cX-width/5-dx, cY+(height/2.81)-dy, width/3.5, width/1.6,0,90, fill = 'dimGray')
+    drawArc(cX+width/5-dx, cY+(height/2.81)-dy, width/3.5, width/1.6,270,90, fill = 'dimGray')
+    drawArc(cX+width/5-dx, cY+(height/2.81)-dy, width/3.5, width/1.6,0,90, fill = 'dimGray')
+    drawRect(cX-width/2-dx,cY+(height/6)-dy, width, width/2, fill='black')
+    drawRect(cX-width/2-dx,cY-dy, width, width/3, fill='grey')
+    drawRect(cX-width/2-dx,cY-(height/5.5)-dy, width, width*2, fill='grey')
+    drawRect(cX-width/2-dx,cY-(height/7)-dy, width, width/2, fill='black')
+    drawRect(cX-width/2-dx,cY-(height/4)-dy, width, width*1.2, fill='black')
+    drawRect(cX-width/4-dx,cY-height/3-dy, width/20, height/1.6, fill='silver')
+    
+    if engineOn and app.thrustPercent > 0:
+        thrustDecimal = app.thrustPercent/100
+        drawPolygon(cX-width/12, cY+(height/2.81),cX-width/12 - width/3.5, cY+(height/2.81), cX-width/12 - width/7,cY+(height/2.81)+width*2.2*(thrustDecimal), fill=gradient('white', 'gold', start='top'), opacity = 80)
+        drawPolygon(cX+width/12, cY+(height/2.81),cX+width/12 + width/3.5, cY+(height/2.81), cX+width/12 + width/7,cY+(height/2.81)+width*2.2*(thrustDecimal), fill=gradient('white', 'gold', start='top'), opacity = 80)
